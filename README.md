05a9a9e6b10ac7fd4b1018977c4f9a4cf2144b41
Oswald Puplampu <oswald.puplampu@lowcosttravelgroup.com>
2016-05-12 16:21:30 +0100

    added karma test configuration

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # angular2Samples
+
+
+# watch for file changes using typescript 
+
+# For app
+tsc --watch --p src/app
+# For e2e-tests
+tsc --watch --p e2e-tests
---
+++ karma.conf.js
@@ -0,0 +1,30 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : './',
+
+    files : [
+      'src/app/components/**/*.js',
+      'src/app/view*/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
